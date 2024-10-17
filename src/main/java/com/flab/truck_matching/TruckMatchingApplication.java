6c823b587c71446e7b8271b28ca25f5631033455
riderkaman <riderkaman@gmail.com>
2024-10-17 23:52:56 +0900

    first commit

---
+++ src/main/java/com/flab/truck_matching/TruckMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.flab.truck_matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckMatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/truck_matching/TruckMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.truck_matching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
