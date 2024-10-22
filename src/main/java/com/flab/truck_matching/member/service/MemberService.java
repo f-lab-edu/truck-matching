c77e059d1bdf6f98f05436bfa3d8fa24c9fccdda
riderkaman <riderkaman@gmail.com>
2024-10-22 20:31:39 +0900

    회원가입 비밀번호 저장 시 인코딩해서 넣도록 수정

--- src/main/java/com/flab/truck_matching/member/service/MemberService.java
+++ src/main/java/com/flab/truck_matching/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.flab.truck_matching.member.domain.Member;
 import com.flab.truck_matching.member.dto.MemberDTO;
 import com.flab.truck_matching.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,9 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder encoder;
 
     @Transactional
     public String addMember(MemberDTO.RegisterReq req) {
+        req.setPassword(encoder.encode(req.getPassword()));
+
         Member member = Member.builder().req(req).build();
         memberRepository.save(member);
         return member.getMemberId();
